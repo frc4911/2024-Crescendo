51e57fc97f65cbc0fa91bc92a707236e6e89bfdb
Riley Brewer <riley.brewer@gmail.com>
2023-10-25 19:52:51 -0700

    Fix build errors
    remove photon and ctre stuff (will add later if necessary)

--- src/main/java/com/cyberknights4911/ctre/PigeonFactory.java
+++
@@ -1,8 +0,0 @@
-package com.cyberknights4911.ctre;
-
-import com.ctre.phoenix.sensors.BasePigeon;
-
-/** Factory for creating BasePigeon objects. */
-public interface PigeonFactory {
-    BasePigeon create(int id);
-}
