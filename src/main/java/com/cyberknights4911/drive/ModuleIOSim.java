a8f648f9f3b5af1036c01ffaa8344cd840d6727a
Riley Brewer <riley.brewer@gmail.com>
2024-01-17 16:44:15 -0800

    Refactoring drive code
    * Moved IO implementations to respective robot packages
    * Moved subsystems.drive to drive

--- src/main/java/com/cyberknights4911/drive/ModuleIOSim.java
+++ src/main/java/com/cyberknights4911/drive/ModuleIOSim.java
@@ -5,7 +5,7 @@
 // license that can be found in the LICENSE file at
 // the root directory of this project.
 
-package com.cyberknights4911.subsystems.drive;
+package com.cyberknights4911.drive;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
