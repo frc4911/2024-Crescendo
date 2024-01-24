5be42d4a55d69ac683b48ff8a819f0e8d47ed874
Caleb Brewer <118789120+SuperDuperGreen@users.noreply.github.com>
2024-01-23 19:43:21 -0800

    added encoders

--- src/main/java/com/cyberknights4911/robot2024/collect/CollectIO.java
+++ src/main/java/com/cyberknights4911/robot2024/collect/CollectIO.java
@@ -13,8 +13,10 @@ public interface CollectIO {
 
   @AutoLog
   public static class CollectIOInputs {
-    public double positionRad = 0.0;
-    public double velocityRadPerSec = 0.0;
+    public double positionRadLeft = 0.0;
+    public double positionRadRight = 0.0;
+    public double velocityRadPerSecLeft = 0.0;
+    public double velocityRadPerSecRight = 0.0;
     public double appliedVoltsLeft = 0.0;
     public double appliedVoltsRight = 0.0;
     public double currentAmpsLeft = 0.0;
