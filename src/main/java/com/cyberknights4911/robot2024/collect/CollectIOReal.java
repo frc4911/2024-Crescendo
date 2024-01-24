e54684d67a9a512dcaba47f2bc0f0448ec8524e6
Caleb Brewer <118789120+SuperDuperGreen@users.noreply.github.com>
2024-01-23 19:56:41 -0800

    Revert "Revert "added encoders""
    This reverts commit b4fa03d39fc57ddb1a9ccd4936b0147fb38b0073.

--- src/main/java/com/cyberknights4911/robot2024/collect/CollectIOReal.java
+++ src/main/java/com/cyberknights4911/robot2024/collect/CollectIOReal.java
@@ -9,14 +9,25 @@ package com.cyberknights4911.robot2024.collect;
 
 import com.revrobotics.CANSparkFlex;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.math.util.Units;
 
 public class CollectIOReal implements CollectIO {
+
+  // TODO: modify this value to match that of the actual collector
+  private static final double GEAR_RATIO = 1.0;
   private final CANSparkFlex collectLeft;
   private final CANSparkFlex collectRight;
+
+  private final RelativeEncoder encoderRight;
+  private final RelativeEncoder encoderLeft;
 
   public CollectIOReal() {
     collectLeft = new CANSparkFlex(0, MotorType.kBrushless);
     collectRight = new CANSparkFlex(0, MotorType.kBrushless);
+
+    encoderRight = collectRight.getEncoder();
+    encoderLeft = collectLeft.getEncoder();
 
     configureDevices();
   }
@@ -29,6 +40,12 @@ public class CollectIOReal implements CollectIO {
 
   @Override
   public void updateInputs(CollectIOInputs inputs) {
+    inputs.positionRadRight = Units.rotationsToRadians(encoderRight.getPosition()) / GEAR_RATIO;
+    inputs.velocityRadPerSecRight =
+        Units.rotationsPerMinuteToRadiansPerSecond(encoderRight.getVelocity()) / GEAR_RATIO;
+    inputs.positionRadLeft = Units.rotationsToRadians(encoderLeft.getPosition()) / GEAR_RATIO;
+    inputs.velocityRadPerSecLeft =
+        Units.rotationsPerMinuteToRadiansPerSecond(encoderLeft.getVelocity()) / GEAR_RATIO;
     inputs.appliedVoltsLeft = collectLeft.getAppliedOutput() * collectLeft.getBusVoltage();
     inputs.appliedVoltsRight = collectRight.getAppliedOutput() * collectRight.getBusVoltage();
     inputs.currentAmpsLeft = collectLeft.getOutputCurrent();
@@ -36,6 +53,7 @@ public class CollectIOReal implements CollectIO {
   }
 
   private void configureDevices() {
+
     collectLeft.restoreFactoryDefaults();
     collectRight.restoreFactoryDefaults();
 
