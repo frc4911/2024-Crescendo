16d26e6d6fe1a5af24aaa555e18dbc0d58823466
Riley Brewer <riley.brewer@gmail.com>
2024-01-23 19:16:47 -0800

    Add encoder usage to collector

--- src/main/java/com/cyberknights4911/robot2024/collect/CollectIOReal.java
+++ src/main/java/com/cyberknights4911/robot2024/collect/CollectIOReal.java
@@ -9,14 +9,23 @@ package com.cyberknights4911.robot2024.collect;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.math.util.Units;
 
 public class CollectIOReal implements CollectIO {
+  // TODO: modify this value to match that of the actual collector
+  private static final double GEAR_RATIO = 1.0;
+
   private final CANSparkMax collectLeft;
   private final CANSparkMax collectRight;
+
+  private final RelativeEncoder encoder;
 
   public CollectIOReal() {
     collectLeft = new CANSparkMax(0, MotorType.kBrushless);
     collectRight = new CANSparkMax(0, MotorType.kBrushless);
+
+    encoder = collectRight.getEncoder();
   }
 
   @Override
@@ -27,6 +36,9 @@ public class CollectIOReal implements CollectIO {
 
   @Override
   public void updateInputs(CollectIOInputs inputs) {
+    inputs.positionRad = Units.rotationsToRadians(encoder.getPosition()) / GEAR_RATIO;
+    inputs.velocityRadPerSec =
+        Units.rotationsPerMinuteToRadiansPerSecond(encoder.getVelocity()) / GEAR_RATIO;
     inputs.appliedVoltsLeft = collectLeft.getAppliedOutput() * collectLeft.getBusVoltage();
     inputs.appliedVoltsRight = collectRight.getAppliedOutput() * collectRight.getBusVoltage();
     inputs.currentAmpsLeft = collectLeft.getOutputCurrent();
