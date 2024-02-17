8dc012a6ad064978890db4987aee601ccf70d5a7
Caleb Brewer <118789120+SuperDuperGreen@users.noreply.github.com>
2024-02-17 15:12:25 -0800

    Added roll

--- src/main/java/com/cyberknights4911/drive/GyroIO.java
+++ src/main/java/com/cyberknights4911/drive/GyroIO.java
@@ -18,6 +18,10 @@ public interface GyroIO {
     public double[] odometryYawTimestamps = new double[] {};
     public Rotation2d[] odometryYawPositions = new Rotation2d[] {};
     public double yawVelocityRadPerSec = 0.0;
+    public Rotation2d rollPosition = new Rotation2d();
+    public double[] odometryRollTimestamps = new double[] {};
+    public Rotation2d[] odometryRollPositions = new Rotation2d[] {};
+    public double rollVelocityRadPerSec = 0.0;
   }
 
   public default void updateInputs(GyroIOInputs inputs) {}
