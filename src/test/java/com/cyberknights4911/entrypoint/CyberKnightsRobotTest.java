e90ed7b1419dae29c1864662b7d6e89929b06abc
Riley Brewer <riley.brewer@gmail.com>
2024-04-04 17:51:06 -0700

    add tests

---
+++ src/test/java/com/cyberknights4911/entrypoint/CyberKnightsRobotTest.java
@@ -0,0 +1,19 @@
+// Copyright (c) 2024 FRC 4911
+// https://github.com/frc4911
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package com.cyberknights4911.entrypoint;
+
+import javax.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+public class CyberKnightsRobotTest {
+
+  @Inject CyberKnightsRobot robot;
+
+  @Test
+  public void main() {}
+}
